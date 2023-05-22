35012be00e9b5f31c1eb9d29042aa3480ec34529
Thabo <thabo.radebe@live.com>
2023-05-22 18:17:24 +0200

    cart button

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,16 +2,21 @@ import { FaChurch } from "react-icons/fa";
 import { useGlobalContext } from "./context";
 const Navbar = () => {
   const { totalAmount } = useGlobalContext();
+  const buttonHandle = () => {
+    console.log("button pressed");
+  };
   return (
     <nav>
       <div className="nav-center">
         <h4>Cape Town Branch Atendanceny Regisire App</h4>
-        <div className="nav-container">
+        <button className="cart-button" onClick={buttonHandle}>
           <FaChurch className="cart-icon" />
-          <div className="amount-container">
-            <p className="total-amount">{totalAmount}</p>
+          <div className="nav-container">
+            <div className="amount-container">
+              <p className="total-amount">{totalAmount}</p>
+            </div>
           </div>
-        </div>
+        </button>
       </div>
     </nav>
   );
