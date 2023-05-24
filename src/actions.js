cf0d9b3a217e71f50e505603cdb2816bb7df6d14
Thabo <thabo.radebe@live.com>
2023-05-24 09:55:21 +0200

    card member fort

--- src/actions.js
+++
@@ -1,8 +0,0 @@
-export const CLEAR_CART = "CLEAR_CART";
-export const REMOVE = "REMOVE";
-export const INCREASE = "INCREASE";
-export const DECREASE = "DECREASE";
-export const LOADING = "LOADING";
-export const DISPLAY_ITEMS = "DISPLAY_ITEMS";
-export const NEW_BRANCH_DATE = "NEW_BRANCH_DATE";
-export const UPDATE_ATTENDANCE_RECORD = "UPDATE_ATTENDANCE_RECORD";
